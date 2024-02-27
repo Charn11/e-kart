447976236424b856759370cab9fc65a3a53b0d7d
Charn11 <gc66357@gmail.com>
2024-02-28 01:05:37 +0530

    added basic header

---
+++ src/components/header.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    return(
+        <>
+            <p>E-kart</p>
+            <p>E-Kart, Do You Need Anything Else?</p>
+            <a>HOME</a>
+            <a>SHOP</a>
+            <img src="src/assets/shopping_cart_FILL0_wght400_GRAD0_opsz24.svg"></img>
+        </>
+    )
+}
+
+export default Header;
---
+++ src/components/home.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react'
+import Header from './header';
+
+const Home = () => {
+    return(
+        <Header></Header>
+    )
+}
+
+export default Home;
