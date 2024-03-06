f8ff7c7f68663305e42d517e2eb974fa51e88d34
Charn11 <gc66357@gmail.com>
2024-03-07 01:37:37 +0530

    edit slider

--- src/components/slideshow.jsx
+++ src/components/slideshow.jsx
@@ -2,13 +2,21 @@ import { useState, useEffect } from 'react'
 import '/src/styles/slideshow.css'
 
 let i=0;
-let images = [{src:'src/assets/img1.jpg', alt:'welcome image'},{src:'src/assets/img2.jpg', alt:'samsung QLED TV'}];
+let images = [{src:'src/assets/img1.jpg', alt:'welcome image'},{src:'src/assets/img2.webp', alt:'samsung QLED TV'}];
 
 function Slideshow(){
 
     const [image, setImage] = useState({src:'', alt:''});
 
     function slider(){
+        if(i===1){
+            return(
+                <div className='content-wrapper'>
+                <div className="slide-content">EXCLUSIVE DEAL!!<br></br>QLED GAMING MONITOR<br></br>30% OFF</div>
+                <img id='slideImg' src={images[i].src} alt={images[i].alt}></img>
+                </div>
+            )
+        }
         return <img id='slideImg' src={images[i].src} alt={images[i].alt}></img>
     }
 
