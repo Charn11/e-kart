56815432011762a466b817fb19e8beab343ee841
Charn11 <gc66357@gmail.com>
2024-03-09 01:59:23 +0530

    edit slides

--- src/components/slideshow.jsx
+++ src/components/slideshow.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import '/src/styles/slideshow.css'
+import { ArrowLeftIcon, ArrowRightIcon } from '@radix-ui/react-icons'
 
 let i=0;
 let images = [{src:'src/assets/img1.jpg', alt:'welcome image'},{src:'src/assets/img2.webp', alt:'samsung QLED TV'}];
@@ -12,12 +13,15 @@ function Slideshow(){
         if(i===1){
             return(
                 <div className='content-wrapper'>
-                <div className="slide-content">EXCLUSIVE DEAL!!<br></br>QLED GAMING MONITOR<br></br>30% OFF</div>
-                <img id='slideImg' src={images[i].src} alt={images[i].alt}></img>
+                    <div className="slide-content">EXCLUSIVE DEAL!!<br></br>QLED GAMING MONITOR<br></br>30% OFF</div>
                 </div>
             )
         }
-        return <img id='slideImg' src={images[i].src} alt={images[i].alt}></img>
+        return(
+            <div className='no-content-wrapper'>
+                <img src={images[i].src} alt={images[i].alt}></img>
+            </div>
+         )
     }
 
     function handleSlide(val){
@@ -41,10 +45,18 @@ function Slideshow(){
     return(
         <section>
             <div id='slider-wrapper'>
-                {slider()}
+                <div className='arrow-wrapper'>
+                    <div className='left'>
+                        <ArrowLeftIcon width={30} height={30}></ArrowLeftIcon>
+                    </div>
+                    {slider()}
+                    <div className='right'>
+                        <ArrowRightIcon width={30} height={30}></ArrowRightIcon>
+                    </div>
+                </div>
                 <ol>
-                <li onClick={() => {handleSlide(1)}}></li>
-                <li></li>
+                    <li onClick={() => {handleSlide(1)}}></li>
+                    {/*<li></li>*/}
                 </ol>
             </div>
         </section>
