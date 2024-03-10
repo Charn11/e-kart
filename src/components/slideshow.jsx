3936fc0fe754fa5c70c9667be87646f72e0fdaa8
Charn11 <gc66357@gmail.com>
2024-03-10 22:30:15 +0530

    finished slider

--- src/components/slideshow.jsx
+++ src/components/slideshow.jsx
@@ -4,21 +4,17 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@radix-ui/react-icons'
 
 let i=0;
 let images = [{src:'src/assets/img1.jpg', alt:'welcome image'},{src:'src/assets/img2.webp', alt:'samsung QLED TV'}];
+let initialX, initialY, initialTime;
 
 function Slideshow(){
 
     const [image, setImage] = useState({src:'', alt:''});
+    const [slide, setSlide] = useState(1);
 
     function slider(){
-        if(i===1){
-            return(
-                <div className='content-wrapper'>
-                    <div className="slide-content">EXCLUSIVE DEAL!!<br></br>QLED GAMING MONITOR<br></br>30% OFF</div>
-                </div>
-            )
-        }
         return(
-            <div className='no-content-wrapper'>
+            <div className='no-content-wrapper' onTouchStart={(e) => {handleTouchStart(e)}} 
+            onTouchEnd={(e) => {handleTouchEnd(e)}}>
                 <img src={images[i].src} alt={images[i].alt}></img>
             </div>
          )
@@ -28,6 +24,47 @@ function Slideshow(){
         i=val-1;
         setImage({...image,src: images[i].src, alt: images[i].alt});
         slider();
+        setSlide(i+1);
+    }
+
+    function handleArrow(str){
+        if(str==="left"){
+            i = i-1;
+            if(i<0){
+                i=images.length-1;
+            }
+            setImage({...image,src: images[i].src, alt: images[i].alt});
+            slider();
+            setSlide(i+1);
+        }
+        else{
+            i = i+1;
+            if(i>images.length-1){
+                i=0;
+            }
+            setImage({...image,src: images[i].src, alt: images[i].alt});
+            slider();
+            setSlide(i+1);
+        }
+    }
+
+    function handleTouchStart(e){
+        initialX = e.touches[0].clientX;
+        initialY = e.touches[0].clientY;
+        initialTime = new Date();
+    }
+
+    function handleTouchEnd(e){
+        let deltaX = e.changedTouches[0].clientX - initialX;
+        let deltaY = Math.abs(e.changedTouches[0].clientY - initialY);
+        let deltaTime = new Date() - initialTime;
+
+        if(deltaX <= -30 && deltaY <= 100 && deltaTime <= 300) {
+            handleArrow("left");
+         }
+         else if(deltaX >= 30 && deltaY <= 100 && deltaTime <= 300){
+            handleArrow();
+        }
     }
 
     useEffect(() => {
@@ -37,6 +74,7 @@ function Slideshow(){
                 i=0;
             }
             setImage({...image,src: images[i].src, alt: images[i].alt});
+            setSlide(i+1);
         },5000);
 
         return () =>  clearInterval(interval);
@@ -47,16 +85,16 @@ function Slideshow(){
             <div id='slider-wrapper'>
                 <div className='arrow-wrapper'>
                     <div className='left'>
-                        <ArrowLeftIcon width={30} height={30}></ArrowLeftIcon>
+                        <ArrowLeftIcon width={30} height={30} onClick={() => {handleArrow("left")}}></ArrowLeftIcon>
                     </div>
                     {slider()}
                     <div className='right'>
-                        <ArrowRightIcon width={30} height={30}></ArrowRightIcon>
+                        <ArrowRightIcon width={30} height={30} onClick={() => {handleArrow()}}></ArrowRightIcon>
                     </div>
                 </div>
                 <ol>
-                    <li onClick={() => {handleSlide(1)}}></li>
-                    {/*<li></li>*/}
+                    <li id='bg1' style={slide===1 ? {backgroundColor:'white',opacity:1} : null} onClick={() => {handleSlide(1)}}></li>
+                    <li id='bg2' style={slide===2 ? {backgroundColor:'white',opacity:1} : null} onClick={() => {handleSlide(2)}}></li>
                 </ol>
             </div>
         </section>
